5d487446139014a6fa3bebe29206d90d5cdeaea8
Silas Landricombe <si@siland.co.uk>
2016-02-25 09:20:05 +0000

    Updated some packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -49,7 +49,7 @@ module.exports = function (grunt) {
                 options: {
                     map: true,
                     processors: [
-                        require('autoprefixer-core')({browsers: 'last 1 version'})
+                        require('autoprefixer')({browsers: 'last 1 version'})
                     ]
                 },
                 files: {
